726afdb1413f3f093bc55d0e3a0516af60559b1d
Dave Kane <davelkane@gmail.com>
2020-07-15 23:05:36 -0500

    word_count almost there

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,14 +1,57 @@
 # Your code here
+cache = {}
 
 
 def expensive_seq(x, y, z):
-    # Your code here
-
+    if (x, y, z) in cache:
+        print("cached", (x, y, z))
+        return cache[(x, y, z)]
+    else:
+        if x <= 0:
+            cache[(x, y, z)] = y + z
+            return cache[(x, y, z)]
+        if x > 0:
+            return (
+                expensive_seq(x - 1, y + 1, z)
+                + expensive_seq(x - 2, y + 2, z * 2)
+                + expensive_seq(x - 3, y + 3, z * 3)
+            )
 
 
 if __name__ == "__main__":
-    for i in range(10):
-        x = expensive_seq(i*2, i*3, i*4)
+    for i in range(5):
+        x = expensive_seq(i * 2, i * 3, i * 4)
         print(f"{i*2} {i*3} {i*4} = {x}")
 
     print(expensive_seq(150, 400, 800))
+
+
+# Compute the Expensive Sequence
+
+# It's like the Fibonacci Sequence, but a lot more computationally
+# expensive and a lot less useful.
+
+# ```
+# exps(x, y, z) =
+#      if x <= 0: y + z
+#      if x >  0: exps(x-1,y+1,z) + exps(x-2,y+2,z*2) + exps(x-3,y+3,z*3)
+# ```
+
+# `x`, `y`, and `z` are all greater than or equal to zero.
+
+# This will be tested on inputs as large as:
+
+# ```
+# x = 150
+# y = 400
+# z = 800
+# ```
+
+# Use a hashtable to make sure your solution completes before the universe
+# ends.
+
+# Hint: Va Clguba, n qvpg xrl pna or nal vzzhgnoyr glcr... vapyhqvat n
+# ghcyr.
+
+# (That's encrypted with ROT13--Google `rot13 decoder` to decode it if you
+# want the hint.)
--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,10 +1,79 @@
+from collections import Counter
+
+
 def word_count(s):
     # Your code here
+    # curr_idx = 0
+    # len_of_curr_word = 0
+    # cache = {}
+    # len_input = len(s)
 
+    # # Helper function to dry up the code
+    # def add_to_cache(offset):
+    #     curr_word = s[curr_idx : curr_idx + (len_of_curr_word - offset)]
+    #     print(
+    #         "cache",
+    #         cache,
+    #         "ci",
+    #         curr_idx,
+    #         "locw",
+    #         len_of_curr_word,
+    #         "offset",
+    #         offset,
+    #         "cw",
+    #         curr_word,
+    #     )
+    #     if curr_word in cache:
+    #         cache[curr_word] = +1
+    #     else:
+    #         cache[curr_word] = 1
+
+    # # Iterate through input string one character at a time
+    # for i, char in enumerate(s):
+    #     print(i, char)
+    #     # If the last character of the string is a letter, add the word to the cache
+    #     if i == len_input - 1 and char.isalpha():
+    #         print("end", i, char)
+    #         len_of_curr_word = +1
+    #         add_to_cache(0)
+    #         break
+    #     # If the character is a letter or an apostrophe, add it to the word
+    #     if char.isalpha() or char == "'":
+    #         print("hit", i, char)
+    #         len_of_curr_word += 1
+    #         # print(len_of_curr_word)
+    #     # If the character is not a letter or apostrophe,
+    #     # and the word length is not zero, the word is done,
+    #     # add it to the cache and reset the word length
+    #     else:
+    #         if len_of_curr_word > 0:
+    #             add_to_cache(1)
+    #         len_of_curr_word = 0
+    #     curr_idx += 1
+
+    return dict(
+        Counter(
+            [
+                "".join(
+                    list(
+                        filter(
+                            lambda char: (char.isalpha() or char == "'"),
+                            list(word.lower()),
+                        )
+                    )
+                )
+                for word in s.split(" ")
+            ]
+        )
+    )
 
 
 if __name__ == "__main__":
-    print(word_count(""))
+    print(word_count("Hello    hello"))
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(
+        word_count(
+            "This is a test of the emergency broadcast network. This is only a test."
+        )
+    )
